231bfa0e785f5178b96a444e05d5353ff058bd34
Antoine <adrean337@gmail.com>
2022-05-06 11:53:59 +0200

    added static file in settings + removed test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -72,6 +72,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
