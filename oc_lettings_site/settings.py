a45f974001d628d0cf5a2a0d6b3b493ce4596979
Antoine <adrean337@gmail.com>
2022-05-07 16:02:38 +0200

    testing purposes

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -6,6 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+print(os.environ)
 DEBUG = True
 
 ALLOWED_HOSTS = ["oc-lettings-45.herokuapp.com", "127.0.0.1", "localhost"]
