8a074b7f265ef17516051233a8f187843f9023ed
Antoine <adrean337@gmail.com>
2022-05-07 15:48:56 +0200

    hided secret_key

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -5,7 +5,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["oc-lettings-45.herokuapp.com", "127.0.0.1", "localhost"]
